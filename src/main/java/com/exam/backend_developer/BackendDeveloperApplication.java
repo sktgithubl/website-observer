b9f270d5ab8002177c9461d63ae07e80387d67a7
sktgithubl <skt.sipun123@gmail.com>
2021-06-27 08:55:03 +0530

    initial commit

---
+++ src/main/java/com/exam/backend_developer/BackendDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.exam.backend_developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/backend_developer/BackendDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.backend_developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
