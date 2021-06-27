90b3553ad54b14be6b6df68d2b3cb60fbc047de8
sktgithubl <skt.sipun123@gmail.com>
2021-06-27 09:52:24 +0530

    Scheduler added

--- src/main/java/com/exam/backend_developer/Services/WebsiteService.java
+++ src/main/java/com/exam/backend_developer/Services/WebsiteService.java
@@ -2,17 +2,25 @@ package com.exam.backend_developer.Services;
 
 import com.exam.backend_developer.Model.WebsiteRequest;
 import com.exam.backend_developer.Repository.WebsiteRepository;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
 
 @Service
 public class WebsiteService {
-
     @Autowired
     WebsiteRepository websiteRepository;
 
     public void addWebsite(WebsiteRequest websiteRequest) {
         websiteRepository.save(websiteRequest);
         return;
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void run() {
+        //website check
     }
 }
